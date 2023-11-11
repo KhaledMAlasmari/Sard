3884d1dd40c7f5e3c0ed83b913dd56a053bf16d7
KhaledAlasmari <kalasmari@lucidya.com>
2023-11-11 20:16:58 +0300

    disable buttons and change colors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,7 +18,7 @@ const Sidebar = ({ themeState }: props) => {
     return (
         <Panel position="top-left" style={{ margin: '0px' }} className="flex flex-col bg-gray-900 text-white h-screen w-64">
             <div className="p-4">
-                <h1 className="text-2xl font-bold">Story Genre</h1>
+                <h1 className="text-2xl font-bold">Genre</h1>
                 <select defaultValue={"Disabled"} disabled={true} className="bg-gray-800 mt-2 p-2 rounded-lg w-full">
                     <option>Disabled</option>
                     <option>Fantasy</option>
@@ -28,13 +28,13 @@ const Sidebar = ({ themeState }: props) => {
                 </select>
             </div>
             <div className="p-4">
-                <button onClick={addChapter} className="bg-purple-600 text-white py-2 px-4 rounded-lg w-full">Add Chapter</button>
+                <button onClick={addChapter} className="bg-pink-600 text-white py-2 px-4 rounded-lg w-full">Add Chapter</button>
             </div>
             <div onClick={toggleMode} className="p-4">
                 <button className="bg-blue-600 text-white py-2 px-4 rounded-lg w-full">Switch theme</button>
             </div>
             <div className="p-4">
-                <button className="bg-blue-600 text-white py-2 px-4 rounded-lg w-full">Generate</button>
+                <button disabled className="bg-blue-600 text-white py-2 px-4 rounded-lg  disabled:bg-gray-600 w-full ">Generate story</button>
             </div>
         </Panel>
     );
