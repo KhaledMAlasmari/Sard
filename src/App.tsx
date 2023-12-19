620dc5e2af279d76daa144d9753b311a6ccbccce
KhaledAlasmari <kalasmari@lucidya.com>
2023-12-19 08:18:34 +0300

    override right click behavior

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import Canvas from './Pages/Canvas';
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Canvas />
-    </ThemeProvider>
+    // the height and margin is to make the canvas full screen
+    <div style={{ height: '100%', margin: 0 }} onContextMenu={(e) => {
+      e.preventDefault(); // prevent the default behaviour when right clicked
+    }}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Canvas />
+      </ThemeProvider>
+    </div>
+
   )
 }
-
 export default App;
