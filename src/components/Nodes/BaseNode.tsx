4cb8c392cc667b07f7e182e315e0385ca43060ff
KhaledAlasmari <kalasmari@lucidya.com>
2023-12-19 08:02:54 +0300

    chapters can be resized now!

--- src/components/Nodes/BaseNode.tsx
+++ src/components/Nodes/BaseNode.tsx
@@ -6,19 +6,27 @@ import {
   NodeResizer,
   useNodeId,
   useReactFlow,
+  Node,
+  NodeProps,
+  ResizeParamsWithDirection,
+  ResizeDragEvent,
 } from 'reactflow';
 import acceptedFormats from '../../utils/acceptedFormats';
 import { FileUploader } from '../FileUploader';
-import { useState } from 'react';
-type Props = {
-  children: JSX.Element,
-  BodyClassName: string,
-  TextClassName: string,
-  title: string,
-  uploadIsAllowed: boolean
+import { useRef, useState } from 'react';
+export type Props = {
+  children: JSX.Element;
+  BodyClassName: string;
+  TextClassName: string;
+  title: string;
+  uploadIsAllowed: boolean;
+  data?: any
 };
-function  BaseNode({ children, BodyClassName, TextClassName, title, uploadIsAllowed }: Props) {
+function BaseNode({ children, BodyClassName, TextClassName, title, uploadIsAllowed }: Props) {
   const nodeId = useNodeId()
+  const node = useRef<HTMLDivElement>(null)
+  const [width, setWidth] = useState<number>(node.current?.offsetWidth!)
+  const [height, setHeight] = useState<number>(node.current?.offsetHeight!)
   const instance = useReactFlow();
   const [base64Img, setBase64Img] = useState<String | undefined>("")
   const handleImageUpload = (file: File) => {
@@ -44,11 +52,16 @@ function  BaseNode({ children, BodyClassName, TextClassName, title, uploadIsAllo
     instance.deleteElements({ nodes: [{ id: nodeId! }] });
   }
 
+  const updateHeightAndWidth = (event: ResizeDragEvent, params: ResizeParamsWithDirection) => {
+    setWidth(params.width)
+    setHeight(params.height)
+  }
 
   return (
-    <div >
+    <>
+      {title === 'Chapter' ? <NodeResizer onResize={updateHeightAndWidth} /> : null}
       <Handle type="target" position={Position.Top} />
-      <div id={nodeId!} className={BodyClassName}>
+      <div ref={node} id={nodeId!} className={BodyClassName} style={{ width: width, height: height }}>
         <button onClick={deleteNode} className={`absolute top-2 right-2 p-1 rounded-full bg-red-500 hover:bg-red-600 transition-colors`} />
         {
           uploadIsAllowed ?
@@ -62,8 +75,7 @@ function  BaseNode({ children, BodyClassName, TextClassName, title, uploadIsAllo
         {children}
       </div>
       <Handle type="source" position={Position.Bottom} />
-
-    </ div>
+    </>
 
   )
 }
